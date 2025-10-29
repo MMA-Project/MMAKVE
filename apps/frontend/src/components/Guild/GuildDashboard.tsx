4f9cea846fee5bc35898d65fe73856f44975f0fe
Melvin SIMON <msimon@la-joliverie.com>
2025-10-29 20:56:26 +0100

    ft: navigation page

---
+++ apps/frontend/src/components/Guild/GuildDashboard.tsx
@@ -0,0 +1,7 @@
+export function GuildDashboard() {
+    return (
+        <div className="p-8">
+            <h1 className="text-3xl font-bold mb-4">Tableau de bord de la guilde</h1>
+        </div>
+    );
+}
