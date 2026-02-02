b262c60069c4a86898953fcbb4f126cf88929674
Mattis44 <mattisnaud@outlook.fr>
2026-02-02 08:56:18 +0100

    feat: add endpoints for guild bank and inventory

--- apps/backend/src/routes/guild.route.ts
+++ apps/backend/src/routes/guild.route.ts
@@ -4,3 +4,7 @@ import * as GuildController from "../controllers/guild.controller.js";
 const router: Router = Router();
 
 router.get("/", GuildController.getAllGuilds);
+router.get("/:guildId/bank", GuildController.getGuildBank);
+router.get("/:guildId/inventory", GuildController.getGuildInventory);
+
+export default router;
